eb0dd0abde109787a5ed18e983847a8391e51d86
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-09-13 13:02:45 +0900

    enable coverage

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
+    reporters: ['default', 'junit', 'json'],
+    outputFile: {
+      junit: './logs/vitest-junit.xml',
+      json: './logs/vitest-report.json',
+    },
     coverage: {
       // 'json-summary' is required for Github action
       reporter: ['text', 'json-summary', 'json'],
